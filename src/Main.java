7898ccd5606ad6246e9733595ef0e466025b8a92
pai-puella <cat.dyshkant@yandex.ru>
2023-09-11 21:44:55 +0300

    increased and decreased the values of variables in accordance with tasks 2 and 3

--- src/Main.java
+++ src/Main.java
@@ -13,5 +13,26 @@ public class Main {
         var paper = 763789;
         System.out.println("Значение переменной paper: " + paper);
 
+        System.out.println();
+        System.out.println("Задача 2");
+
+        dog += 4;
+        cat += 4;
+        paper += 4;
+
+        System.out.println("Новое значение переменной dog: " + dog);
+        System.out.println("Новое значение переменной cat: " + cat);
+        System.out.println("Новое значение переменной paper: " + paper);
+
+        System.out.println();
+        System.out.println("Задача 3");
+
+        dog -= 3.5;
+        cat -= 1.6;
+        paper -= 7639;
+
+        System.out.println("Значение переменной dog после уменьшения: " + dog);
+        System.out.println("Значение переменной cat после уменьшения: " + cat);
+        System.out.println("Значение переменной paper после уменьшения: " + paper);
         }
     }
