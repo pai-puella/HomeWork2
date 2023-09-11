f54eb8eabcf4a5657b9e51e827488dcb07c9f7be
pai-puella <cat.dyshkant@yandex.ru>
2023-09-11 21:23:23 +0300

    initial commit

---
+++ src/Main.java
@@ -0,0 +1,17 @@
+
+public class Main {
+    public static void main(String[] args) {
+
+        System.out.println("Задача 1");
+
+        var dog = 8.0;
+        System.out.println("Значение переменной dog: " + dog);
+
+        var cat = 3.6;
+        System.out.println("Значение переменной cat: " + cat);
+
+        var paper = 763789;
+        System.out.println("Значение переменной paper: " + paper);
+
+        }
+    }
